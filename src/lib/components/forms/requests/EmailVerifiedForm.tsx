79d6c33847e635e76d084b06dbd9725ae5baf301
anekra <andika.eka.102@gmail.com>
2025-11-21 23:55:32 +0700

    Fix: change label tag to shadcn Label tag.

--- src/lib/components/forms/requests/EmailVerifiedForm.tsx
+++ src/lib/components/forms/requests/EmailVerifiedForm.tsx
@@ -1,20 +1,21 @@
 "use client";
 
-import Form from "next/form";
 import { Input } from "@/lib/components/ui/input";
+import Form from "next/form";
+import { Label } from "../../ui/label";
 
 export default function EmailVerifiedForm() {
-  return (
-    <Form action="" className="flex flex-col gap-6">
-      <div>
-        <label className="mb-2 flex items-center justify-between text-sm">
-          Display Name
-        </label>
-        <Input placeholder="Your display name" />
-      </div>
-      <button type="submit" className="btn-solid-p">
-        Finish Account
-      </button>
-    </Form>
-  );
+	return (
+		<Form action="" className="flex flex-col gap-6">
+			<div>
+				<Label className="mb-2 flex items-center justify-between text-sm">
+					Display Name
+				</Label>
+				<Input placeholder="Your display name" />
+			</div>
+			<button type="submit" className="btn-solid-p">
+				Finish Account
+			</button>
+		</Form>
+	);
 }
