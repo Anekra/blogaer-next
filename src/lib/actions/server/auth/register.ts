1dac12a5be2c060ffc505416959fe4d95c0ca1be
anekra <andika.eka.102@gmail.com>
2025-11-21 23:47:01 +0700

    Format: imports reformat.

--- src/lib/actions/server/auth/register.ts
+++ src/lib/actions/server/auth/register.ts
@@ -1,37 +1,37 @@
 "use server";
 
-import { z } from "zod";
-
-import { RegisterFormSchema } from "@/lib/types/zodSchemas";
+import type { AuthDto, EncoreErrDto } from "@/lib/types/dto/CommonDto";
+import type { RegisterFormSchema } from "@/lib/types/zodSchemas";
 import { headers } from "next/headers";
-import { AuthDto, EncoreErrDto } from "@/lib/types/dto/CommonDto";
+import type { z } from "zod";
 import setSessionCookie from "./setSessionCookie";
 
 export default async function register(
-  values: z.infer<typeof RegisterFormSchema>
+	values: z.infer<typeof RegisterFormSchema>,
 ) {
-  try {
-    const url: string = `${process.env.API_ROUTE}/auth/register`;
-    const userAgent = (await headers()).get("user-agent");
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "User-Agent": `${userAgent}`,
-        Origin: "http://localhost:3000"
-      },
-      body: JSON.stringify(values)
-    });
+	try {
+		const url: string = `${process.env.API_ROUTE}/auth/register`;
+		const userAgent = (await headers()).get("user-agent");
+		const response = await fetch(url, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				"User-Agent": `${userAgent}`,
+				Origin: "http://localhost:3000",
+			},
+			body: JSON.stringify(values),
+		});
 
-    const resJson = await response.json();
-    if (!response.ok) return resJson as EncoreErrDto;
+		const resJson = await response.json();
+    console.log("register.ts >> resJson:", resJson);
+		if (!response.ok) return resJson as EncoreErrDto;
 
-    const authData = resJson as AuthDto;
-    await setSessionCookie(authData.data);
+		const authData = resJson as AuthDto;
+		await setSessionCookie(authData.data);
 
-    return true;
-  } catch (error) {
-    console.error("register.ts >>>", error);
-    return false;
-  }
+		return true;
+	} catch (error) {
+		console.error("register.ts >>", error);
+		return false;
+	}
 }
