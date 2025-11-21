15904cd30190389976c277ffd9223db261818030
anekra <andika.eka.102@gmail.com>
2025-11-21 23:49:03 +0700

    Improve: adding csrf and exp to Auth type. Refactor: change any to unknown.

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,87 +1,87 @@
+import type { DraftDto, DraftWithNoUserDto } from "@/lib/types/dto/DraftDto";
+import type { PostDto, PostWithNoUserDto } from "@/lib/types/dto/PostDto";
 import type { BaseEditor } from "slate";
 import type { HistoryEditor } from "slate-history";
 import type { ReactEditor } from "slate-react";
 
-import { PostDto, PostWithNoUserDto } from "@/lib/types/dto/PostDto";
-import { DraftDto, DraftWithNoUserDto } from "@/lib/types/dto/DraftDto";
-
 export type Session = {
-  clientId: string;
-  username: string;
-  email: string;
-  role: string;
-  name: string;
-  desc?: string;
-  img?: string;
-  exp?: number;
-}| null;
+	clientId: string;
+	username: string;
+	email: string;
+	role: string;
+	name: string;
+	desc?: string;
+	img?: string;
+	exp?: number;
+} | null;
 
 export type SessionCookie = {
-  username: string;
-  email: string;
-  role: string;
-  img?: string;
+	username: string;
+	email: string;
+	role: string;
+	img?: string;
 } | null;
 
 export type Auth = {
-  clientId: string;
-  csrf: string;
-  username: string;
-  email: string;
-  name: string;
-  role: string;
-  desc?: string;
-  img?: string;
+	clientId: string;
+	username: string;
+	email: string;
+	name: string;
+	role: string;
+	csrf: string;
+	exp?: number;
+	desc?: string;
+	img?: string;
 };
 
 export type RefreshToken = {
-  username: string;
-  clientId: string;
-  csrf: string;
-  exp: number;
+	username: string;
+	clientId: string;
+	csrf: string;
+	exp: number;
 };
 
 export type Draft = {
-  id: string;
-  title: string;
-  content?: any;
-  tags?: string[];
+	id: string;
+	title: string;
+	content?: unknown;
+	tags?: string[];
 };
 
 export type EditPost = {
-  slugOrId: string;
-  title: string;
-  content: any;
-  tags: string[];
+	slugOrId: string;
+	title: string;
+	content: unknown;
+	tags: string[];
 };
 
 export type SearchParams = { param: string; value: string }[];
 
 export type CurrentPost =
-  | PostDto
-  | PostWithNoUserDto
-  | DraftDto
-  | DraftWithNoUserDto;
+	| PostDto
+	| PostWithNoUserDto
+	| DraftDto
+	| DraftWithNoUserDto;
 
 export type SlateEditor = BaseEditor & ReactEditor & HistoryEditor;
 
-export type AnyObj = { [key: string]: any };
+export type AnyObj = { [key: string]: unknown };
 
 export type EncoreErrorCode =
-  | "ok"
-  | "cancelled"
-  | "unknown"
-  | "invalid_argument"
-  | "deadline_exceeded"
-  | "not_found"
-  | "already_exists"
-  | "permission_denied"
-  | "resource_exhausted"
-  | "failed_precondition"
-  | "aborted"
-  | "out_of_range"
-  | "unimplemented"
-  | "internal"
-  | "unavailable"
-  | "data_loss"
-  | "unauthenticated";
+	| "ok"
+	| "cancelled"
+	| "unknown"
+	| "invalid_argument"
+	| "deadline_exceeded"
+	| "not_found"
+	| "already_exists"
+	| "permission_denied"
+	| "resource_exhausted"
+	| "failed_precondition"
+	| "aborted"
+	| "out_of_range"
+	| "unimplemented"
+	| "internal"
+	| "unavailable"
+	| "data_loss"
+	| "unauthenticated";
