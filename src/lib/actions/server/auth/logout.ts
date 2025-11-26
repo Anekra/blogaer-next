b2c5d577d55d85f2f9ef190079e8d9127bf31e2c
anekra <andika.eka.102@gmail.com>
2025-11-26 15:18:27 +0700

    Improve: logout now a semi authenticated route.

--- src/lib/actions/server/auth/logout.ts
+++ src/lib/actions/server/auth/logout.ts
@@ -1,33 +1,44 @@
 "use server";
 
-import { cookies, headers } from "next/headers";
+import type { Session } from "@/lib/types";
 import jwt from "jsonwebtoken";
-import { Session } from "@/lib/types";
+import { cookies, headers } from "next/headers";
 
 export default async function logout() {
-  const cookie = await cookies();
-  const sessionCookie = `${process.env.SESSION}`;
-  const encryptedSession = cookie.get(`${process.env.SESSION}`)?.value;
-  if (!encryptedSession) {
-    cookie.delete(sessionCookie);
-    return;
-  }
-  const session = jwt.verify(
-    encryptedSession,
-    `${process.env.SESSION_SECRET}`
-  ) as Session;
-  if (!session) {
-    cookie.delete(sessionCookie);
-    return;
-  }
-  const url = `${process.env.API_ROUTE}/auth/logout`;
-  const userAgent = (await headers()).get("user-agent");
-  await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "User-Agent": `${userAgent}`,
-      Origin: "http://localhost:3000"
-    }
-  });
+	const cookie = await cookies();
+	const sessionCookie = `${process.env.SESSION}`;
+	const encryptedSession = cookie.get(`${process.env.SESSION}`)?.value;
+	if (!encryptedSession) {
+		cookie.delete(sessionCookie);
+		return;
+	}
+	const decodedSession = jwt.verify(
+		encryptedSession,
+		`${process.env.SESSION_SECRET}`,
+		{ ignoreExpiration: true }
+	) as Session;
+	if (!decodedSession) {
+		cookie.delete(sessionCookie);
+		return;
+	}
+	const url = `${process.env.API_ROUTE}/auth/logout`;
+	const header = await headers();
+	const userAgent = header.get("user-agent");
+	const xForwardedFor = header.get("x-forwarded-for");
+	const csrf = cookie.get(`${process.env.CSRF}`)?.value;
+	if (!csrf) {
+		cookie.delete(sessionCookie);
+		return;
+	}
+	await fetch(url, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"User-Agent": `${userAgent}`,
+			"X-Authorization": `Bearer ${decodedSession.clientId}`,
+			"X-Semi-CSRF": csrf,
+			"X-Forwarded-For": `${xForwardedFor}`,
+			Origin: "http://localhost:3000"
+		}
+	});
 }
