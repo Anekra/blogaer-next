212f03231432c55d9d9fcc94ce1ae2d44191ec84
anekra <andika.eka.102@gmail.com>
2025-11-26 15:26:20 +0700

    Refactor: renaming enum.

--- src/lib/components/dialogs/dialog-contents/EmailStatusDialogContent.tsx
+++ src/lib/components/dialogs/dialog-contents/EmailStatusDialogContent.tsx
@@ -1,47 +1,46 @@
 import {
-  AlignRightIcon,
-  MailIcon,
-  MailCheckIcon,
-  MailXIcon
+	DialogClose,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle
+} from "@/lib/components/ui/dialog";
+import { TempInfo } from "@/lib/utils/enums";
+import {
+	AlignRightIcon,
+	MailCheckIcon,
+	MailIcon,
+	MailXIcon
 } from "lucide-react";
 
-import {
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle
-} from "@/lib/components/ui/dialog";
-import { EmailInfo } from "@/lib/utils/enums";
-
 export default function EmailStatusDialogContent({ info }: { info: string }) {
-  return (
-    <DialogContent>
-      <DialogHeader>
-        <DialogTitle>Email Status</DialogTitle>
-        {!info ? (
-          <div className="flex animate-slide-ltr justify-center">
-            <div className="flex flex-col justify-center">
-              <AlignRightIcon size={50} />
-            </div>
-            <MailIcon size={100} />
-          </div>
-        ) : info === EmailInfo.Success ? (
-          <div className="flex justify-center">
-            <MailCheckIcon size={100} className="text-green-600" />
-          </div>
-        ) : (
-          <div className="flex justify-center">
-            <MailXIcon size={100} className="text-red-600" />
-          </div>
-        )}
-        <DialogDescription className={`${info ? "text-base" : "dots-loading"}`}>
-          {info || "sending email"}
-        </DialogDescription>
-      </DialogHeader>
-      <DialogClose className="btn-solid-base-rounder justify-self-center px-6">
-        Close
-      </DialogClose>
-    </DialogContent>
-  );
+	return (
+		<DialogContent>
+			<DialogHeader>
+				<DialogTitle>Email Status</DialogTitle>
+				{!info ? (
+					<div className="flex animate-slide-ltr justify-center">
+						<div className="flex flex-col justify-center">
+							<AlignRightIcon size={50} />
+						</div>
+						<MailIcon size={100} />
+					</div>
+				) : info === TempInfo.EmailSent ? (
+					<div className="flex justify-center">
+						<MailCheckIcon size={100} className="text-green-600" />
+					</div>
+				) : (
+					<div className="flex justify-center">
+						<MailXIcon size={100} className="text-red-600" />
+					</div>
+				)}
+				<DialogDescription className={`${info ? "text-base" : "dots-loading"}`}>
+					{info || "sending email"}
+				</DialogDescription>
+			</DialogHeader>
+			<DialogClose className="btn-solid-base-rounder justify-self-center px-6">
+				Close
+			</DialogClose>
+		</DialogContent>
+	);
 }
