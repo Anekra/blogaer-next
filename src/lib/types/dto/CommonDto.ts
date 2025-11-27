ba076def00ecb358f004abaa32519a82de62d043
anekra <andika.eka.102@gmail.com>
2025-11-27 08:47:47 +0700

    Changing details property type.

--- src/lib/types/dto/CommonDto.ts
+++ src/lib/types/dto/CommonDto.ts
@@ -5,7 +5,7 @@ import type { PostDto, PostWithNoUserDto } from "./PostDto";
 export type EncoreErrDto = {
 	code: string;
 	message: string;
-	details?: string;
+	details?: unknown;
 };
 
 export type AuthDto = {
