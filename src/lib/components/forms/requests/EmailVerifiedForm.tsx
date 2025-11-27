5681ec2ae6f6a27682841d17d913e84947e0ede5
anekra <andika.eka.102@gmail.com>
2025-11-27 08:31:38 +0700

    Improve: moving client side code to here from EmailStatus page.

--- src/lib/components/forms/requests/EmailVerifiedForm.tsx
+++ src/lib/components/forms/requests/EmailVerifiedForm.tsx
@@ -1,21 +1,73 @@
 "use client";
 
+import userPatch from "@/lib/actions/server/userPatch";
 import { Input } from "@/lib/components/ui/input";
-import Form from "next/form";
-import { Label } from "../../ui/label";
+import { Label } from "@/lib/components/ui/label";
+import { EmailSubject, TempInfo, TempValue } from "@/lib/utils/enums";
+import { useRouter, useSearchParams } from "next/navigation";
+import { type FormEvent, useEffect, useRef } from "react";
+import { toast } from "sonner";
 
 export default function EmailVerifiedForm() {
+	const router = useRouter();
+	const nameRef = useRef<HTMLInputElement>(null);
+	const params = useSearchParams();
+	const request = params.get("request");
+	const verified = params.get("verified");
+	const message = params.get("message");
+
+	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const name = nameRef.current?.value || "";
+		const response = await userPatch({ name });
+		if (response.message) {
+			sessionStorage.setItem(TempValue.ToastMsg, TempInfo.LoginSuccess);
+			router.replace("/home");
+		} else {
+			toast.error(response.error, {
+				position: "bottom-right",
+				duration: 1500
+			});
+		}
+	};
+
+	useEffect(() => {
+		if (typeof window !== "undefined") {
+			sessionStorage.setItem(TempValue.ToastMsg, TempInfo.VerifyEmailSuccess);
+			if (verified === "true") {
+				toast.success(TempInfo.VerifyEmailSuccess, {
+					position: "bottom-right",
+					duration: 1500
+				});
+				sessionStorage.removeItem(TempValue.ToastMsg);
+			}
+		}
+	}, [verified]);
+
+	if (request !== EmailSubject.VerifyEmail) {
+		router.replace("/");
+	}
+
+	if (!verified) {
+		return <p>{message}</p>;
+	}
+
 	return (
-		<Form action="" className="flex flex-col gap-6">
+		<form method="post" className="flex flex-col gap-6" onSubmit={handleSubmit}>
 			<div>
 				<Label className="mb-2 flex items-center justify-between text-sm">
 					Display Name
 				</Label>
-				<Input placeholder="Your display name" />
+				<Input
+					ref={nameRef}
+					type="text"
+					name="name"
+					placeholder="Your display name"
+				/>
 			</div>
 			<button type="submit" className="btn-solid-p">
 				Finish Account
 			</button>
-		</Form>
+		</form>
 	);
 }
