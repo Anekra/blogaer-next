5167d114464f5d163cbe3595521f15f530e22b08
anekra <andika.eka.102@gmail.com>
2025-12-06 11:41:13 +0700

    Reformat: removing unused format.

--- src/app/(user)/(withsidebar)/home/page.tsx
+++ src/app/(user)/(withsidebar)/home/page.tsx
@@ -1,7 +1,5 @@
-import React from "react";
-
 import PostCardsHolderA from "@/lib/components/cards/holders/PostCardsHolderA";
 
 export default function HomePage() {
-  return <PostCardsHolderA />;
+	return <PostCardsHolderA />;
 }
